bf140fd284d81761f43c9e2ee684b1462d1ec81f
Daniel Fiser <danfis@danfis.cz>
2023-06-28 17:22:08 +0200

    Formatting

--- include/pheromone/policy_server.h
+++ include/pheromone/policy_server.h
@@ -14,16 +14,15 @@ extern "C" {
 #endif /* __cplusplus */
 
 typedef char *(*phrm_policy_req_fdr_task_fd)(size_t *size, void *userdata);
-typedef int (*phrm_policy_req_fdr_operator)(const int *state,
-                                               void *userdata);
+typedef int (*phrm_policy_req_fdr_operator)(const int *state, void *userdata);
 
 /**
  * Runs the server and blocks as long as the server is running
  */
 int phrmPolicyServer(const char *url,
-                        phrm_policy_req_fdr_task_fd req_fdr_fd,
-                        phrm_policy_req_fdr_operator req_fdr_op,
-                        void *userdata);
+                     phrm_policy_req_fdr_task_fd req_fdr_fd,
+                     phrm_policy_req_fdr_operator req_fdr_op,
+                     void *userdata);
 
 #ifdef __cplusplus
 }
